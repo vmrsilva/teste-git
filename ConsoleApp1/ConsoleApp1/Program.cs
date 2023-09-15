b7f7d8c7a3e44e67adf1d6918e2e5d9e72ea6331
Valmir <50370623+vmrsilva@users.noreply.github.com>
2023-09-14 21:28:17 -0300

    nova mensagem.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Digite o seu nome:");
+var nome = Console.ReadLine();
+
+
+Console.WriteLine($"Hello, {nome}!");
